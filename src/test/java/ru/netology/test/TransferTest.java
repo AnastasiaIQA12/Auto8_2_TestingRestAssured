135dbcc0e8abe882763b3bbd3b00b13e242ad730
AnastasiaIQA12 <Sinka8900@mail.ru>
2021-02-23 16:10:59 +0400

    Change old Test and add new Test

--- src/test/java/ru/netology/test/TransferTest.java
+++ src/test/java/ru/netology/test/TransferTest.java
@@ -21,6 +21,46 @@ public class TransferTest {
     }
 
     @Test
+    void shouldTransferZeroOnFirstCard()  {
+        int sum = 0;
+        val authInfo = getAuthInfo();
+        ApiHelper.postQueryUser(authInfo);
+        val verificationCode = getVerificationCode(authInfo);
+        String token = ApiHelper.getToken(verificationCode);
+        Card[] cards= ApiHelper.getCard(token);
+        balance2 = Integer.parseInt(cards[0].getBalance());
+        balance1 = Integer.parseInt(cards[1].getBalance());
+        numberCard1= DataHelper.getFirstCard().getNumber();
+        numberCard2= DataHelper.getSecondCard().getNumber();
+        ApiHelper.madeTransfer(token, numberCard2, numberCard1, sum);
+        Card[] cardsAfterTransfer =ApiHelper.getCard(token);
+        int endBalance2 = Integer.parseInt(cardsAfterTransfer[0].getBalance());
+        int endBalance1 = Integer.parseInt(cardsAfterTransfer[1].getBalance());
+        assertEquals(balance1+sum, endBalance1);
+        assertEquals(balance2-sum, endBalance2);
+    }
+
+    @Test
+    void shouldTransferZeroOnSecondCard()  {
+        int sum = 0;
+        val authInfo = getAuthInfo();
+        ApiHelper.postQueryUser(authInfo);
+        val verificationCode = getVerificationCode(authInfo);
+        String token = ApiHelper.getToken(verificationCode);
+        Card[] cards= ApiHelper.getCard(token);
+        balance2 = Integer.parseInt(cards[0].getBalance());
+        balance1 = Integer.parseInt(cards[1].getBalance());
+        numberCard1= DataHelper.getFirstCard().getNumber();
+        numberCard2= DataHelper.getSecondCard().getNumber();
+        ApiHelper.madeTransfer(token, numberCard1, numberCard2, sum);
+        Card[] cardsAfterTransfer =ApiHelper.getCard(token);
+        int endBalance2 = Integer.parseInt(cardsAfterTransfer[0].getBalance());
+        int endBalance1 = Integer.parseInt(cardsAfterTransfer[1].getBalance());
+        assertEquals(balance1-sum, endBalance1);
+        assertEquals(balance2+sum, endBalance2);
+    }
+
+    @Test
     void shouldTransferMiddleOnFirstCard()  {
         int sum = 5000;
         val authInfo = getAuthInfo();
@@ -28,14 +68,14 @@ public class TransferTest {
         val verificationCode = getVerificationCode(authInfo);
         String token = ApiHelper.getToken(verificationCode);
         Card[] cards= ApiHelper.getCard(token);
-        balance1 = Integer.parseInt(cards[0].getBalance());
-        balance2 = Integer.parseInt(cards[1].getBalance());
+        balance2 = Integer.parseInt(cards[0].getBalance());
+        balance1 = Integer.parseInt(cards[1].getBalance());
         numberCard1= DataHelper.getFirstCard().getNumber();
         numberCard2= DataHelper.getSecondCard().getNumber();
         ApiHelper.madeTransfer(token, numberCard2, numberCard1, sum);
         Card[] cardsAfterTransfer =ApiHelper.getCard(token);
-        int endBalance1 = Integer.parseInt(cardsAfterTransfer[0].getBalance());
-        int endBalance2 = Integer.parseInt(cardsAfterTransfer[1].getBalance());
+        int endBalance2 = Integer.parseInt(cardsAfterTransfer[0].getBalance());
+        int endBalance1 = Integer.parseInt(cardsAfterTransfer[1].getBalance());
         assertEquals(balance1+sum, endBalance1);
         assertEquals(balance2-sum, endBalance2);
     }
@@ -48,16 +88,98 @@ public class TransferTest {
         val verificationCode = getVerificationCode(authInfo);
         String token = ApiHelper.getToken(verificationCode);
         Card[] cards= ApiHelper.getCard(token);
-        balance1 = Integer.parseInt(cards[0].getBalance());
-        balance2 = Integer.parseInt(cards[1].getBalance());
+        balance2 = Integer.parseInt(cards[0].getBalance());
+        balance1 = Integer.parseInt(cards[1].getBalance());
         numberCard1= DataHelper.getFirstCard().getNumber();
         numberCard2= DataHelper.getSecondCard().getNumber();
         ApiHelper.madeTransfer(token, numberCard1, numberCard2, sum);
         Card[] cardsAfterTransfer =ApiHelper.getCard(token);
-        int endBalance1 = Integer.parseInt(cardsAfterTransfer[0].getBalance());
-        int endBalance2 = Integer.parseInt(cardsAfterTransfer[1].getBalance());
+        int endBalance2 = Integer.parseInt(cardsAfterTransfer[0].getBalance());
+        int endBalance1 = Integer.parseInt(cardsAfterTransfer[1].getBalance());
         assertEquals(balance1-sum, endBalance1);
         assertEquals(balance2+sum, endBalance2);
     }
+
+    @Test
+    void shouldTransferMaximumOnFirstCard()  {
+        int sum = 10000;
+        val authInfo = getAuthInfo();
+        ApiHelper.postQueryUser(authInfo);
+        val verificationCode = getVerificationCode(authInfo);
+        String token = ApiHelper.getToken(verificationCode);
+        Card[] cards= ApiHelper.getCard(token);
+        balance2 = Integer.parseInt(cards[0].getBalance());
+        balance1 = Integer.parseInt(cards[1].getBalance());
+        numberCard1= DataHelper.getFirstCard().getNumber();
+        numberCard2= DataHelper.getSecondCard().getNumber();
+        ApiHelper.madeTransfer(token, numberCard2, numberCard1, sum);
+        Card[] cardsAfterTransfer =ApiHelper.getCard(token);
+        int endBalance2 = Integer.parseInt(cardsAfterTransfer[0].getBalance());
+        int endBalance1 = Integer.parseInt(cardsAfterTransfer[1].getBalance());
+        assertEquals(balance1+sum, endBalance1);
+        assertEquals(balance2-sum, endBalance2);
+    }
+
+    @Test
+    void shouldTransferMaximumOnSecondCard()  {
+        int sum = 10000;
+        val authInfo = getAuthInfo();
+        ApiHelper.postQueryUser(authInfo);
+        val verificationCode = getVerificationCode(authInfo);
+        String token = ApiHelper.getToken(verificationCode);
+        Card[] cards= ApiHelper.getCard(token);
+        balance2 = Integer.parseInt(cards[0].getBalance());
+        balance1 = Integer.parseInt(cards[1].getBalance());
+        numberCard1= DataHelper.getFirstCard().getNumber();
+        numberCard2= DataHelper.getSecondCard().getNumber();
+        ApiHelper.madeTransfer(token, numberCard1, numberCard2, sum);
+        Card[] cardsAfterTransfer =ApiHelper.getCard(token);
+        int endBalance2 = Integer.parseInt(cardsAfterTransfer[0].getBalance());
+        int endBalance1 = Integer.parseInt(cardsAfterTransfer[1].getBalance());
+        assertEquals(balance1-sum, endBalance1);
+        assertEquals(balance2+sum, endBalance2);
+    }
+
+    @Test
+    void shouldTransferMoreThanMaximumOnFirstCard()  {
+        int sum = 20000;
+        val authInfo = getAuthInfo();
+        ApiHelper.postQueryUser(authInfo);
+        val verificationCode = getVerificationCode(authInfo);
+        String token = ApiHelper.getToken(verificationCode);
+        Card[] cards= ApiHelper.getCard(token);
+        balance2 = Integer.parseInt(cards[0].getBalance());
+        balance1 = Integer.parseInt(cards[1].getBalance());
+        numberCard1= DataHelper.getFirstCard().getNumber();
+        numberCard2= DataHelper.getSecondCard().getNumber();
+        ApiHelper.madeTransfer(token, numberCard2, numberCard1, sum);
+        Card[] cardsAfterTransfer =ApiHelper.getCard(token);
+        int endBalance2 = Integer.parseInt(cardsAfterTransfer[0].getBalance());
+        int endBalance1 = Integer.parseInt(cardsAfterTransfer[1].getBalance());
+        assertEquals(balance1, endBalance1);
+        assertEquals(balance2, endBalance2);
+    }
+
+    @Test
+    void shouldTransferMoreThanMaximumOnSecondCard()  {
+        int sum = 20000;
+        val authInfo = getAuthInfo();
+        ApiHelper.postQueryUser(authInfo);
+        val verificationCode = getVerificationCode(authInfo);
+        String token = ApiHelper.getToken(verificationCode);
+        Card[] cards= ApiHelper.getCard(token);
+        balance2 = Integer.parseInt(cards[0].getBalance());
+        balance1 = Integer.parseInt(cards[1].getBalance());
+        numberCard1= DataHelper.getFirstCard().getNumber();
+        numberCard2= DataHelper.getSecondCard().getNumber();
+        ApiHelper.madeTransfer(token, numberCard1, numberCard2, sum);
+        Card[] cardsAfterTransfer =ApiHelper.getCard(token);
+        int endBalance2 = Integer.parseInt(cardsAfterTransfer[0].getBalance());
+        int endBalance1 = Integer.parseInt(cardsAfterTransfer[1].getBalance());
+        assertEquals(balance1, endBalance1);
+        assertEquals(balance2, endBalance2);
+    }
+
+
 
 }
